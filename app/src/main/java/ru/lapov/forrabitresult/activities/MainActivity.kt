6df4f952fed461583f755e1bf348683449f05459
Denis <denisnovikov94@gmail.com>
2018-12-28 21:11:11 +0300

    fix design

--- app/src/main/java/ru/lapov/forrabitresult/activities/MainActivity.kt
+++ app/src/main/java/ru/lapov/forrabitresult/activities/MainActivity.kt
@@ -30,14 +30,6 @@ class MainActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
         initList()
         addList()
